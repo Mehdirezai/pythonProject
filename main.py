a28d39e8ba4ba6e5adac1da2ca3c0671ffc25bb6
nima.rayan <mehdi75rezai1401@gmail.com>
2023-09-12 11:07:45 +0330

    first learn

---
+++ main.py
@@ -0,0 +1,13 @@
+listOfStudents = ['mehdi', 'ali', 'sara', 'mina', 'aryan']
+listOfScores = [20, 17, 9, 11, 10]
+//sara ->
+
+for i in range(0,5):
+    if listOfScores[i] >= 10 :
+        print (listOfStudents[i]+" قبول ")
+    if listOfScores[i]  <10:
+
+       (listOfStudents[i]+' مردود ')
+
+
+
