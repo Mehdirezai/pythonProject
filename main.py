d68d6a56e19d070b7b00267df721a94f296211a5
nima.rayan <mehdi75rezai1401@gmail.com>
2023-09-12 11:13:20 +0330

    this code show variable of (a) that it is a simple var=27

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-listOfStudents = ['mehdi', 'ali', 'sara', 'mina', 'aryan']
-listOfScores = [20, 17, 9, 11, 10]
-//sara ->
-
-for i in range(0,5):
-    if listOfScores[i] >= 10 :
-        print (listOfStudents[i]+" قبول ")
-    if listOfScores[i]  <10:
-
-       (listOfStudents[i]+' مردود ')
-
-
+a=27
+print(a)
 
